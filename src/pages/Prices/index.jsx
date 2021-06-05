8d637197ed5f0742a59ff2916e1832d762843de8
Katka Martínková <martinkova.katie@gmail.com>
2021-06-05 16:16:34 +0200

    imported Prices and Registration pages

--- src/pages/Prices/index.jsx
+++ src/pages/Prices/index.jsx
@@ -1,5 +1,97 @@
 import React from 'react';
+import './style.css';
 
 export const Prices = () => {
-  return <div>pricelist here</div>;
+  const imgUrl = '../../assets/pesZmenseno.jpg';
+
+  return (
+    <>
+      {/* <!--Sekce galerie a cenik--> */}
+      <div className="kontejner">
+        <section className="fotogalerieCenik" id="dvereGalerie">
+          <h2>Galerie a ceník poskytovaných služeb</h2>
+          <p>
+            Balíček <span style={{ color: '#ee9b00' }}>FREE</span>
+            <br />
+            Po přihlášení do uživatelského účtu lze zobrazit historicky poslední
+            uložený snímek z kamery. Tato služba je zdarma.
+          </p>
+          <p>
+            Balíček <span style={{ color: '#ee9b00' }}>STANDARD</span>
+            <br />
+            Po přihlášení do uživatelského účtu lze zobrazit historicky poslední
+            uložený snimek z kamery. V minuatuře dalších pět předešlých snímků.
+            V historii si lze vybrat snímky za posledních 30 dnů.
+          </p>
+
+          <div className="obrazkyDiv">
+            <table
+              className="aktualniSnimek"
+              style={{ border: '2' }}
+              height=" 400px"
+              width="350px"
+            >
+              <tbody>
+                <tr>
+                  <td>
+                    <img className="obrazekPes" src={imgUrl} />
+                  </td>
+                </tr>
+                <tr>
+                  <td>aktualni snimek</td>
+                </tr>
+              </tbody>
+            </table>
+            <div className="miniaturyDiv">
+              <table
+                className="miniatury"
+                style={{ border: '2' }}
+                height="200px"
+                width="650px"
+              >
+                <tbody>
+                  <tr>
+                    <td>
+                      <img className="obrazekPes" src={imgUrl} />
+                    </td>
+                    <td>
+                      <img className="obrazekPes" src={imgUrl} />
+                    </td>
+                    <td>
+                      <img className="obrazekPes" src={imgUrl} />
+                    </td>
+                    <td>
+                      <img className="obrazekPes" src={imgUrl} />
+                    </td>
+                    <td>
+                      <img className="obrazekPes" src={imgUrl} />
+                    </td>
+                  </tr>
+                  <tr className="textKObrazkum">
+                    <td>obrazek c. 1</td>
+                    <td>obrazek c. 2</td>
+                    <td>obrazek c. 3</td>
+                    <td>obrazek c. 4</td>
+                    <td>obrazek c. 5</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            {/* <!-- <ul className="fotkyCenik">
+          <li className="obrazekDvereOriginal"src="obrazky/reddoor.jpg" alt="dvere"></li>
+          <li className="obrazekPes"src="obrazky/pes zmenšeno.jpg" alt="pes na pohovce"></li>
+          <li className="obrazekPes"src="obrazky/pes zmenšeno.jpg" alt="pes na pohovce"></li>
+          <li className="obrazekPes"src="obrazky/pes zmenšeno.jpg" alt="pes na pohovce"></li>
+          <li className="obrazekPes"src="obrazky/pes zmenšeno.jpg" alt="pes na pohovce"></li>
+        </ul>--> */}
+
+            {/* <!-- <img className="obrazekPes" src="obrazky/pes zmenšeno.jpg" alt="dvere" />
+      <img className="obrazekPes" src="obrazky/pes zmenšeno.jpg" alt="dvere" />
+      <img className="obrazekPes" src="obrazky/pes zmenšeno.jpg" alt="dvere" />
+      <img className="obrazekPes" src="obrazky/pes zmenšeno.jpg" alt="dvere" />--> */}
+          </div>
+        </section>
+      </div>
+    </>
+  );
 };
--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -1,5 +1,131 @@
 import React from 'react';
+import './style.css';
 
 export const Registration = () => {
-  return <div>registration here</div>;
+  return (
+    <>
+      <div className="kontejner">
+        <h3>REGISTRACE Vášeho účtu YOUR WEBCAM</h3>
+        <p>
+          S účtem Your Webcam budete jednoduše ovládat a spravovat Vaší historii
+          snímků s těmito výhodami:
+        </p>
+        <div className="obrazRegistrace">
+          <ul>
+            <li>Jednoduché ovládání po zadání e-mailové adresy a hesla.</li>
+            <li>Přehled a sledování snímku ve vámi určený časový horizont.</li>
+            <li>Možnost zaslání vybraných snímků na libovolný e-mail.</li>
+          </ul>
+          <img
+            className="imgRegistrace"
+            src="../../assets/registrace.jpg"
+            alt="registrace uzivatele"
+          />
+        </div>
+        <hr />
+        <form method="POST" className="formularRegistrace" action="https://">
+          {/* <!--Zacatek poli uctu--> */}
+          <fieldset>
+            <h3>Jsem nový zákazník</h3>
+            <ul className="formularovePole">
+              <li>
+                <label htmlFor="jmeno">Jméno: </label>
+                <input
+                  type="text"
+                  id="jmeno"
+                  name="jmeno"
+                  className="velke"
+                  required="required"
+                  placeholder="Zadejte své jméno"
+                />
+              </li>
+              <li>
+                <label htmlFor="prijmeni">Příjmení: </label>
+                <input
+                  type="text"
+                  id="prijmeni"
+                  name="prijmeni"
+                  className="velke"
+                />
+              </li>
+              <li>
+                <label htmlFor="email">E-mail: </label>
+                <input type="email" id="email" name="email" className="velke" />
+              </li>
+              <li>
+                <label htmlFor="heslo">Heslo: </label>
+                <input type="heslo" id="heslo" name="heslo" className="velke" />
+              </li>
+            </ul>
+          </fieldset>
+          {/* <!--Konec poli uctu--> */}
+          {/* <!--Zacatak poli adresy--> */}
+          <fieldset>
+            <h3>Adresa</h3>
+            <ul className="formularovePole">
+              <li>
+                <label htmlFor="ulice_a cislo_domu">Ulice a číslo domu: </label>
+                <input
+                  type="text"
+                  id="ulice_a cislo_domu"
+                  name="ulice_a cislo_domu"
+                  className="velke"
+                />
+              </li>
+              <li>
+                <label htmlFor="mesto">Město: </label>
+                <input type="text" id="mesto" name="mesto" className="velke" />
+              </li>
+              {/* <!--<li>
+              <label htmlFor="stat">Stát: </label>
+              <select id="stat" name="stat">
+                <option value="CZ" className="velke">Česká republika</option>
+                <option value="SK">Slovensko</option>
+                <option value="DE">Něměcko</option>
+                <option value="Pl">Polsko</option>
+              </select>
+            </li>--> */}
+              <li>
+                <label htmlFor="psc">PSČ: </label>
+                <input type="text" id="psc" name="psc" className="male" />
+              </li>
+            </ul>
+          </fieldset>
+          {/* <!--Konec pole adresy--> */}
+          {/* <!--Zacatek vyber sluzby--> */}
+          <fieldset>
+            <h3>Výběr poskytovaného balíčku služeb</h3>
+            <ul className="formularovePole">
+              <li className="buttonVyber">
+                <label htmlFor="typ_sluzby">Free </label>
+                <input type="radio" value="souhlasim" id="typ_sluzby" />
+              </li>
+              <li className="buttonVyber">
+                <label htmlFor="typ_sluzby">Standard </label>
+                <input type="radio" value="souhlasim" id="typ_sluzby" />
+              </li>
+              <li className="buttonVyber">
+                <label htmlFor="typ_sluzby">Premium</label>
+                <input type="radio" value="souhlasim" id="typ_sluzby" />
+              </li>
+              <li className="buttonVyber">
+                <label htmlFor="typ_sluzby">
+                  Vyzkoušet Premium na 14 dní ZDARMA
+                </label>
+                <input type="radio" value="souhlasim" id="typ_sluzby" />
+              </li>
+            </ul>
+          </fieldset>
+          {/* <!--Konec vyberu sluzby--> */}
+          <fieldset>
+            <input
+              type="submit"
+              className="vytvorit_ucet"
+              value="Vytvořit účet"
+            />
+          </fieldset>
+        </form>
+      </div>
+    </>
+  );
 };
